563dfb4e91dddeda3bc24aec279f9edce6d91405
Sal Pal <salman.s.iqbal@gmail.com>
2018-01-25 21:50:43 -0600

    Combined completed project2 with validation attempt code

--- Project2_With_Validation.cpp
+++ Project2_With_Validation.cpp
@@ -2,18 +2,16 @@
 
 using namespace std;
 
-#include <limits>
-
 int main() {
-    //std::cout << "Hello, World!" << std::endl;
-    //return 0;
-    int userInput;
+    int userInput, sumPos=0, sumNeg=0, sumAll=0;
     const int arraySize = 10;
     int array[arraySize];
 
+    cout << "You will be entering 10 whole numbers\nPlease hit 'enter' after each entry.\n";
     for (int i = 0; i <= 9; i++) {
-        cout << "Input any whole number: " << endl;
-        cin >> array[i];
+
+        cout << "Please enter any whole number: "<< endl;
+        //cin >> array[i];
 
         while(!(cin >> array[i])) {
             cout << "That is not a whole number! Please enter a whole number: " << endl;
@@ -24,11 +22,25 @@ int main() {
             cin >> array[i];
            // cin >> array[i];
         }
-        //cout << "You entered " << array[i] << "." << endl;
-
     }
     cout << "You entered these 10 numbers: ";
     for (int i = 0; i <= 9; i++) {
-        cout << array[i] << ", ";
+        cout << array[i] << " ";
     }
+
+    for (int i = 0; i <= 9; i++) {
+
+        if (array[i] > 0) {
+            sumPos += array[i];
+        } else {
+            sumNeg += array[i];
+        }
+        sumAll += array[i];
+    }
+    cout << endl;
+    cout << array[4];
+    cout << "The sum of all numbers greater than zero is: " << sumPos << endl;
+    cout << "The sum of all numbers less than zero is: " << sumNeg << endl;
+    cout << "The sum of all numbers is: " << sumAll << endl;
+
 }
